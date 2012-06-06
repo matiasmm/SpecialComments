7fa6b2ec2fe40f697f4949fbef28628ef60d2aa2
Matias M. Montenegro <matiasmmontenegro@gmail.com>
2012-06-06 10:41:20 -0300

    Corrected twig generator with themes

--- emitters/twig/TwigTemplateCreator.php
+++ emitters/twig/TwigTemplateCreator.php
@@ -3,16 +3,6 @@ class TwigTemplateCreator extends TemplateCreator{
     public $original_file_subfix = ".html";
     public $output_file_subfix = ".html.twig";
     
-    protected static $twig;
-    
-    static function twigToPhp($string){
-            $loader = new Twig_Loader_String();
-            $twig = new Twig_Environment($loader);
-            $twig->setCompiler(new TemplateTwigCompiler($twig));
-        
-        $module = $twig->parse($twig->tokenize($string, null)); 
-        return $twig->compile($module);
-    }
     
     /**
      * converts the tree of parsed nodes into a php file
@@ -25,15 +15,14 @@ class TwigTemplateCreator extends TemplateCreator{
         $base = str_replace($this->original_file_subfix, '',basename($file_node->getAttribute('file')));
         foreach($file_node->themes as $theme){
             $file_o = sprintf('%s/generated-helpers/%s.%s%s',$output_dir, $base, $theme, $this->output_file_subfix);
-            file_put_contents($file_o, $twig->compileSource($file_node->render()));
-            echo shell_exec("php -l ". $file_o);
+            file_put_contents($file_o, $file_node->render());
+	    printf("\n  Generated %s\n", $file_o);
         }
 
         if(empty($file_node->themes)){
             $file_o = sprintf('%s/generated-helpers/%s%s',$output_dir, $base, $this->output_file_subfix);
             file_put_contents($file_o, $file_node->render());
-            echo shell_exec("php -l ". $file_o);
-
+	    printf("\n  Generated %s\n", $file_o);
         }
     }
 }
